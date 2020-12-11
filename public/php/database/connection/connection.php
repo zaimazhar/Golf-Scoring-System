0472c1e5799df7dea0e8d7b36335417cebab39b4
zaimazhar97 <zaim.azhar97@gmail.com>
2020-12-11 19:58:31 +0800

    Update query execution method

--- public/php/database/connection/connection.php
+++ public/php/database/connection/connection.php
@@ -3,6 +3,7 @@
 class pgConnection {
 
     private $dsn;
+    private $currStmt;
 
     protected function __construct() {
         $this->dsn = new PDO("pgsql:dbname=GolfSystem;host=localhost", "xxxx", "xxxx");
@@ -10,7 +11,17 @@ class pgConnection {
         return $this->dsn;
     }
 
-    protected function executeQuery() {
+    protected function executeQuery(string $query = null, $value_to_store = null) {
+        if(!$query) return;
 
+        $this->currStmt = $this->dsn->prepare($query);
+
+        if(!empty($value_to_store)) {
+            $this->currStmt->execute();
+        } else {
+            $this->currStmt->execute($value_to_store);
+        }
+
+        return $this->currStmt;
     }
 }
