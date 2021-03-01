3bd11f1e03ad0f17c4394f59849705815a11fb04
zaimazhar97 <54124001+zaimazhar97@users.noreply.github.com>
2021-03-01 21:51:26 +0800

    Add prompt checking value

--- public/routes/resources/index.js
+++ public/routes/resources/index.js
@@ -1,10 +1,13 @@
 console.log("Hello World");
 
 function venueDeleteConfirmation(vname, vid) {
-    vname = prompt(`Type ${vname} 
+    let vname_store = prompt(`Type ${vname} 
     to delete`)
-    let deleteForm = new XMLHttpRequest()
-    deleteForm.open("GET", `http://golf_score_system.io/posts/venue_delete?vid=${vid}`)
 
-
+    if(vname_store === vname) {
+        let deleteForm = new XMLHttpRequest()
+        deleteForm.open("GET", `http://golf_score_system.io/posts/venue_delete?vid=${vid}`)
+    } else {
+        console.log("You SUCKS")
+    }
 }
