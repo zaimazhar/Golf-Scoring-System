d4948a05e96ee3e05471288013b08847b98299f8
zaimazhar97 <54124001+zaimazhar97@users.noreply.github.com>
2021-03-05 10:23:45 +0800

    Score insertion with par and stableford

--- public/routes/participant.php
+++ public/routes/participant.php
@@ -29,9 +29,7 @@ echo $unfilled_hole . "<br>";
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once("./components/head.php") ?>
     <title><?= $participant['name'] ?></title>
 </head>
 <body>
